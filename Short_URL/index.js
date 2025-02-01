c24d9501cebf6ddd68f8a7a598fa74f9cfb07408
Aman Kushwaha <109275885+amankushwaha100@users.noreply.github.com>
2025-02-01 12:19:43 +0530

    Server Side Rendering with EJS and Node.Js

--- Short_URL/index.js
+++ Short_URL/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const path = require("path");
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
+const staticRouter = require("./routes/staticRouter");
 const URL = require('./models/url');
 const app = express();
 const PORT = 8001;
@@ -9,11 +11,17 @@ connectToMongoDB('mongodb://127.0.0.1:27017/short-url')
 .then(() => console.log('Mongodb connected')
 )
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"))
+
 app.use(express.json())
 
-app.use("/url", urlRoute);
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/:shortId', async (req, res) => {
+app.use("/url", urlRoute);
+app.use("/", staticRouter);
+
+app.get('/url/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
         shortId
